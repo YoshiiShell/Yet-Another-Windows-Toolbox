336b5c9d887ed99b229fd3c8d717ff46fc95cc45
YoshiiShell <160530303+YoshiiShell@users.noreply.github.com>
2024-04-25 23:08:34 -0400

    Regroup package deletions

--- WindowsToolbox.bat
+++ WindowsToolbox.bat
@@ -1242,13 +1242,17 @@ echo Windows-System-Diagnostics-Telemetry-PlatformTelemetryClient
 powershell -ExecutionPolicy Unrestricted "Get-AppxPackage -AllUsers -Name Windows-System-Diagnostics-Telemetry-PlatformTelemetryClient | ForEach-Object { $_.Name }"
 
 echo Disable telemetry packages
+echo OneCore
 powershell -ExecutionPolicy Unrestricted "Get-WindowsPackage -Online -PackageName '* Microsoft-OneCoreUAP-Feedback-StringFeedbackEngine*' | Remove-WindowsPackage -Online"
-powershell -ExecutionPolicy Unrestricted "Get-WindowsPackage -Online -PackageName '* Microsoft-Windows-ErrorReporting-Adm-Deployment-LanguagePack*' | Remove-WindowsPackage -Online"
+powershell -ExecutionPolicy Unrestricted "Get-WindowsPackage -Online -PackageName '* Microsoft-OneCore-SystemSettings-InputCloudStore*' | Remove-WindowsPackage -Online"
+echo Windows Error reporting packages
 powershell -ExecutionPolicy Unrestricted "Get-WindowsPackage -Online -PackageName '* Microsoft-Windows-ErrorReportingConsole*' | Remove-WindowsPackage -Online"
 powershell -ExecutionPolicy Unrestricted "Get-WindowsPackage -Online -PackageName '* Microsoft-Windows-ErrorReportingConsole.Resources*' | Remove-WindowsPackage -Online"
 powershell -ExecutionPolicy Unrestricted "Get-WindowsPackage -Online -PackageName '* Microsoft-Windows-ErrorReportingCore*' | Remove-WindowsPackage -Online"
 powershell -ExecutionPolicy Unrestricted "Get-WindowsPackage -Online -PackageName '* Microsoft-Windows-ErrorReportingDumpTypeControl*' | Remove-WindowsPackage -Online"
 powershell -ExecutionPolicy Unrestricted "Get-WindowsPackage -Online -PackageName '* Microsoft-Windows-ErrorReportingDumpTypeControl-Deployment*' | Remove-WindowsPackage -Online"
+powershell -ExecutionPolicy Unrestricted "Get-WindowsPackage -Online -PackageName '* Microsoft-Windows-ErrorReporting-Adm-Deployment*' | Remove-WindowsPackage -Online"
+echo Windows Feedback Engine
 powershell -ExecutionPolicy Unrestricted "Get-WindowsPackage -Online -PackageName '* Microsoft-Windows-Feedback-CourtesyEngine*' | Remove-WindowsPackage -Online"
 powershell -ExecutionPolicy Unrestricted "Get-WindowsPackage -Online -PackageName '* Microsoft-Windows-Feedback-DeploymentMgrClient*' | Remove-WindowsPackage -Online"
 powershell -ExecutionPolicy Unrestricted "Get-WindowsPackage -Online -PackageName '* Microsoft-Windows-Feedback-DeploymentMgrClient-Desktop-TaskSch*' | Remove-WindowsPackage -Online"
@@ -1256,7 +1260,7 @@ powershell -ExecutionPolicy Unrestricted "Get-WindowsPackage -Online -PackageNam
 powershell -ExecutionPolicy Unrestricted "Get-WindowsPackage -Online -PackageName '* Microsoft-Windows-FeedbackNotifications-Adm.Resources*' | Remove-WindowsPackage -Online"
 powershell -ExecutionPolicy Unrestricted "Get-WindowsPackage -Online -PackageName '* Microsoft-Windows-Feedback-Service*' | Remove-WindowsPackage -Online"
 powershell -ExecutionPolicy Unrestricted "Get-WindowsPackage -Online -PackageName '* Microsoft-Windows-Feedback-Service.Resources*' | Remove-WindowsPackage -Online"
-powershell -ExecutionPolicy Unrestricted "Get-WindowsPackage -Online -PackageName '* Microsoft-OneCore-SystemSettings-InputCloudStore*' | Remove-WindowsPackage -Online"
+echo Windows Application Experience
 powershell -ExecutionPolicy Unrestricted "Get-WindowsPackage -Online -PackageName '* Microsoft-Windows-Application-Experience-AIT-Static*' | Remove-WindowsPackage -Online"
 powershell -ExecutionPolicy Unrestricted "Get-WindowsPackage -Online -PackageName '* Microsoft-Windows-Application-Experience-AppInv*' | Remove-WindowsPackage -Online"
 powershell -ExecutionPolicy Unrestricted "Get-WindowsPackage -Online -PackageName '* Microsoft-Windows-Application-Experience-Core-Inventory-Service*' | Remove-WindowsPackage -Online"
@@ -1266,6 +1270,7 @@ powershell -ExecutionPolicy Unrestricted "Get-WindowsPackage -Online -PackageNam
 powershell -ExecutionPolicy Unrestricted "Get-WindowsPackage -Online -PackageName '* Microsoft-Windows-Application-Experience-Program-Data*' | Remove-WindowsPackage -Online"
 powershell -ExecutionPolicy Unrestricted "Get-WindowsPackage -Online -PackageName '* Microsoft-Windows-Application-Experience-Program-Data.Resources*' | Remove-WindowsPackage -Online"
 powershell -ExecutionPolicy Unrestricted "Get-WindowsPackage -Online -PackageName '* Microsoft-Windows-CodeIntegrity-Aggregator*' | Remove-WindowsPackage -Online"
+echo Windows Compat Appraiser
 powershell -ExecutionPolicy Unrestricted "Get-WindowsPackage -Online -PackageName '* Microsoft-Windows-Compat-Appraiser*' | Remove-WindowsPackage -Online"
 powershell -ExecutionPolicy Unrestricted "Get-WindowsPackage -Online -PackageName '* Microsoft-Windows-Compat-Appraiser.Resources*' | Remove-WindowsPackage -Online"
 powershell -ExecutionPolicy Unrestricted "Get-WindowsPackage -Online -PackageName '* Microsoft-Windows-Compat-Appraiser-InboxDataFiles*' | Remove-WindowsPackage -Online"
@@ -1275,17 +1280,26 @@ powershell -ExecutionPolicy Unrestricted "Get-WindowsPackage -Online -PackageNam
 powershell -ExecutionPolicy Unrestricted "Get-WindowsPackage -Online -PackageName '* Microsoft-Windows-Compat-CompatTelRunner-DailyTask*' | Remove-WindowsPackage -Online"
 powershell -ExecutionPolicy Unrestricted "Get-WindowsPackage -Online -PackageName '* Microsoft-Windows-Compat-GeneralTel*' | Remove-WindowsPackage -Online"
 powershell -ExecutionPolicy Unrestricted "Get-WindowsPackage -Online -PackageName '* Microsoft-Windows-Compatibility-Aggregator*' | Remove-WindowsPackage -Online"
+echo Data Collection
 powershell -ExecutionPolicy Unrestricted "Get-WindowsPackage -Online -PackageName '* Microsoft-Windows-DataCollection-Adm*' | Remove-WindowsPackage -Online"
 powershell -ExecutionPolicy Unrestricted "Get-WindowsPackage -Online -PackageName '* Microsoft-Windows-DataCollection-Adm.Resources*' | Remove-WindowsPackage -Online"
+echo Device Census
 powershell -ExecutionPolicy Unrestricted "Get-WindowsPackage -Online -PackageName '* Microsoft-Windows-DeviceCensus-Schedule-ClientServer*' | Remove-WindowsPackage -Online"
+echo Keyboard Diagnostics
 powershell -ExecutionPolicy Unrestricted "Get-WindowsPackage -Online -PackageName '* Microsoft-Windows-KeyboardDiagnostic*' | Remove-WindowsPackage -Online"
 powershell -ExecutionPolicy Unrestricted "Get-WindowsPackage -Online -PackageName '* Microsoft-Windows-KeyboardDiagnostic.Resources*' | Remove-WindowsPackage -Online"
+echo Media Foundation Aggregator
 powershell -ExecutionPolicy Unrestricted "Get-WindowsPackage -Online -PackageName '* Microsoft-Windows-MediaFoundation-MediaFoundationAggregator*' | Remove-WindowsPackage -Online"
+echo Energy Estimation
 powershell -ExecutionPolicy Unrestricted "Get-WindowsPackage -Online -PackageName '* Microsoft-Windows-Power-EnergyEstimationEngine-Client-Overrides*' | Remove-WindowsPackage -Online"
+ehco "Security-PwdlessPlat-Aggregator"
 powershell -ExecutionPolicy Unrestricted "Get-WindowsPackage -Online -PackageName '* Microsoft-Windows-Security-PwdlessPlat-Aggregator*' | Remove-WindowsPackage -Online"
+echo Autologger
 powershell -ExecutionPolicy Unrestricted "Get-WindowsPackage -Online -PackageName '* Microsoft-Windows-SetupPlatform-Telemetry-AutoLogger*' | Remove-WindowsPackage -Online"
+echo System Settings SIUF handlers
 powershell -ExecutionPolicy Unrestricted "Get-WindowsPackage -Online -PackageName '* Microsoft-Windows-SystemSettings-SettingsHandlers-SIUF*' | Remove-WindowsPackage -Online"
 powershell -ExecutionPolicy Unrestricted "Get-WindowsPackage -Online -PackageName '* Microsoft-Windows-SystemSettings-SettingsHandlers-SIUF.Resources*' | Remove-WindowsPackage -Online"
+echo Telemetry Clients
 powershell -ExecutionPolicy Unrestricted "Get-WindowsPackage -Online -PackageName '* Microsoft-Windows-TelemetryClient*' | Remove-WindowsPackage -Online"
 powershell -ExecutionPolicy Unrestricted "Get-WindowsPackage -Online -PackageName '* Microsoft-Windows-Unified-Telemetry-Client*' | Remove-WindowsPackage -Online"
 powershell -ExecutionPolicy Unrestricted "Get-WindowsPackage -Online -PackageName '* Microsoft-Windows-Unified-Telemetry-Client.resources*' | Remove-WindowsPackage -Online"
@@ -1293,19 +1307,23 @@ powershell -ExecutionPolicy Unrestricted "Get-WindowsPackage -Online -PackageNam
 powershell -ExecutionPolicy Unrestricted "Get-WindowsPackage -Online -PackageName '* Microsoft-Windows-Unified-Telemetry-Client-Decoder-Host*' | Remove-WindowsPackage -Online"
 powershell -ExecutionPolicy Unrestricted "Get-WindowsPackage -Online -PackageName '* Microsoft-Windows-Unified-Telemetry-Client-Settings-WindowsClient*' | Remove-WindowsPackage -Online"
 powershell -ExecutionPolicy Unrestricted "Get-WindowsPackage -Online -PackageName '* Microsoft-Windows-Unified-Telemetry-Client-WoWOnly*' | Remove-WindowsPackage -Online"
-powershell -ExecutionPolicy Unrestricted "Get-WindowsPackage -Online -PackageName '* Microsoft-Windows-Update-Aggregators*' | Remove-WindowsPackage -Online"
 powershell -ExecutionPolicy Unrestricted "Get-WindowsPackage -Online -PackageName '* Windows-System-Diagnostics-Telemetry-PlatformTelemetryClient*' | Remove-WindowsPackage -Online"
+echo AC Proxy
 powershell -ExecutionPolicy Unrestricted "Get-WindowsPackage -Online -PackageName '* Microsoft-Windows-AcProxy*' | Remove-WindowsPackage -Online"
 powershell -ExecutionPolicy Unrestricted "Get-WindowsPackage -Online -PackageName '* Microsoft-Windows-AcProxy.Resources*' | Remove-WindowsPackage -Online"
+Customer Experience Improvement Program
 powershell -ExecutionPolicy Unrestricted "Get-WindowsPackage -Online -PackageName '* Microsoft-Windows-CEIPEnable-Adm*' | Remove-WindowsPackage -Online"
 powershell -ExecutionPolicy Unrestricted "Get-WindowsPackage -Online -PackageName '* Microsoft-Windows-CEIPEnable-Adm.Resources*' | Remove-WindowsPackage -Online"
+powershell -ExecutionPolicy Unrestricted "Get-WindowsPackage -Online -PackageName '* Microsoft-Windows-UsbCeip*' | Remove-WindowsPackage -Online"
+powershell -ExecutionPolicy Unrestricted "Get-WindowsPackage -Online -PackageName '* Microsoft-Windows-UsbCeip.Resources*' | Remove-WindowsPackage -Online"
+echo SQM
 powershell -ExecutionPolicy Unrestricted "Get-WindowsPackage -Online -PackageName '* Microsoft-Windows-Client-SQM-Consolidator*' | Remove-WindowsPackage -Online"
 powershell -ExecutionPolicy Unrestricted "Get-WindowsPackage -Online -PackageName '* Microsoft-Windows-SQMApi*' | Remove-WindowsPackage -Online"
 powershell -ExecutionPolicy Unrestricted "Get-WindowsPackage -Online -PackageName '* Microsoft-Windows-SQM-Consolidator-Base*' | Remove-WindowsPackage -Online"
 powershell -ExecutionPolicy Unrestricted "Get-WindowsPackage -Online -PackageName '* Microsoft-Windows-SQM-Consolidator-Base.Resources*' | Remove-WindowsPackage -Online"
-powershell -ExecutionPolicy Unrestricted "Get-WindowsPackage -Online -PackageName '* Microsoft-Windows-UsbCeip*' | Remove-WindowsPackage -Online"
-powershell -ExecutionPolicy Unrestricted "Get-WindowsPackage -Online -PackageName '* Microsoft-Windows-UsbCeip.Resources*' | Remove-WindowsPackage -Online"
-powershell -ExecutionPolicy Unrestricted "Get-WindowsPackage -Online -PackageName '* Microsoft-Windows-ErrorReporting-Adm-Deployment*' | Remove-WindowsPackage -Online"
+echo Intel Indeo Codecs
+powershell -ExecutionPolicy Unrestricted "Get-WindowsPackage -Online -PackageName '* Microsoft-Windows-Indeo4-Codecs*' | Remove-WindowsPackage -Online"
+powershell -ExecutionPolicy Unrestricted "Get-WindowsPackage -Online -PackageName '* Microsoft-Windows-Indeo5-Codecs*' | Remove-WindowsPackage -Online"
 
 echo Disallow Windows Update to reinstall bloat
 reg add "HKLM\SOFTWARE\Microsoft\Windows\CurrentVersion\Appx\AppxAllUserStore\Deprovisioned\Microsoft.3DBuilder_8wekyb3d8bbwe" /f
